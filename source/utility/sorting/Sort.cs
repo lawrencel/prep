0b7ff455cbe8e4945e6ff019a9dbd9870b86d346
Lawrence <lawrencel@iqmetrix.com>
2017-03-21 14:50:44 -0500

    Pushing new changes

--- source/utility/sorting/Sort.cs
+++ source/utility/sorting/Sort.cs
@@ -11,7 +11,19 @@ namespace code.utility.sorting
       return (a, b) => order(accessor(a), accessor(b));
     }
 
-    public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor,
+      public static ICompareTwoItems<Item> by_descending<Property>(IGetTheValueOfAProperty<Item, Property> accessor)
+          where Property : IComparable<Property>
+      {
+          return @by(accessor, SortOrders.@descending);
+      }
+
+        public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor)
+            where Property : IComparable<Property>
+        {
+            return @by(accessor, SortOrders.@ascending);
+        }
+
+        public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor,
       params Property[] sort_order)
     {
       return (a, b) => Array.IndexOf(sort_order, accessor(a)) - Array.IndexOf(sort_order, accessor(b));
