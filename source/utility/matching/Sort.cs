f6c56b319feb463c3145d59ddfe61469eff432ea
Lawrence <lawrencel@iqmetrix.com>
2017-03-21 13:47:04 -0500

    Pushing new changes

---
+++ source/utility/matching/Sort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace code.utility.matching
+{
+    public static class Sort<Item>
+    {
+        public static ICompareTwoItems<Item> by_descending<Property>(IGetTheValueOfAProperty<Item,Property> propertyGetter)
+            where Property : IComparable<Property>
+        {
+            return (first, second) => propertyGetter(second).CompareTo(propertyGetter(first));
+        }
+    }
+}
