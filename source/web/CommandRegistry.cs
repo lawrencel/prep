9cef142755d6ede635b91b9e578b6dbdb9031b25
Lawrence <lawrencel@iqmetrix.com>
2017-03-22 13:32:44 -0500

    Pushing new changes

--- source/web/CommandRegistry.cs
+++ source/web/CommandRegistry.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace code.web
 {
   public class CommandRegistry :IFindACommandThatCanHandleARequest
   {
+      private readonly IEnumerable<IHandleOneWebRequest> _requestHandlers;
+
+      public CommandRegistry(IEnumerable<IHandleOneWebRequest> requestHandlers)
+      {
+          _requestHandlers = requestHandlers;
+      }
      public IHandleOneWebRequest get_command_that_can_handle(IProvideDetailsAboutAWebRequest request)
      {
-       throw new NotImplementedException();
+         return _requestHandlers.First(x=>x.can_process(request));
      }
   }
 }
