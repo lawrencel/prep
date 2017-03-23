acd81510c5e8ee2d62910419b346f759f0ce623a
Lawrence <lawrencel@iqmetrix.com>
2017-03-23 16:22:56 -0500

    Pushing new changes

---
+++ source/utility.containers/TypeFactoryFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code.utility.containers
+{
+    public class TypeFactoryFinder : IFindFactoriesForAType
+    {
+        public static IDictionary<Type, IResolveType> dictionary
+        {
+            get
+            {
+                throw new NotImplementedException("Replace this in constructor ;)");
+            }
+        }
+        public void get_resolver_for_type(Type the_type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/utility.containers/TypeFactoryFinderSpecs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using developwithpassion.specifications.assertions.core;
+using developwithpassion.specifications.assertions.interactions;
+using Machine.Specifications;
+using Rhino.Mocks;
+using spec = developwithpassion.specifications.use_engine<Machine.Fakes.Adapters.Rhinomocks.RhinoFakeEngine>;
+
+namespace code.utility.containers
+{
+    [Subject(typeof(TypeFactoryFinder))]
+    public class TypeFactoryFinderSpecs 
+    {
+        public abstract class concern : spec.observe<IFindFactoriesForAType, TypeFactoryFinder>
+        {
+            
+        }
+
+        public class when_getting_resolver_for_type : concern
+        {
+            private Establish e = () =>
+            {
+                requestedType = typeof(SomeFunnyType);
+                spec.change(() => TypeFactoryFinder.dictionary).to(new Dictionary<Type, IResolveType>());
+            };
+
+            private Because b = () =>
+                sut.get_resolver_for_type(requestedType);
+
+            It creates_dictionary_key_for_requested_type = () =>
+                dictionary.should().received(x=> x.Add(requestedType,Arg<IResolveType>.Is.Anything));
+
+            private static IDictionary<Type, IResolveType> dictionary;
+            private static Type requestedType;
+        }
+
+        public class SomeFunnyType
+        {
+            
+        }
+    }
+
+
+}
